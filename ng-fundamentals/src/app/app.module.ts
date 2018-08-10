d748444705ec0c339ee168163c7578a15f70155c
Alexis Alulema <alexis.alulema@gmail.com>
2018-08-10 06:54:00 -0500

    Adding resolver to service

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -2,27 +2,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from "@angular/router";
 
+import * as LocalLib from './events/index'
+
 import {EventsAppComponent} from './events/events-app.component';
-import {EventsListComponent} from './events/events-list.component';
-import {EventsThumbnailComponent} from './events/events-thumbnail.component';
 import {NavbarComponent} from './navbar/navbar.component';
-import {EventService} from './events/shared/event.service';
 import {ToastrService} from './common/toastr.service.service';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
 import {appRoutes} from "../routes";
-import {CreateEventComponent} from './create-event/create-event.component';
 import {Error404Component} from './errors/errors.component';
-import {EventRouteActivator} from "./events/event-details/event-route-activator.service";
-import {EventsListResolver} from "./events/events-list-resolver.service";
 
 @NgModule({
     declarations: [
         EventsAppComponent,
-        EventsListComponent,
-        EventsThumbnailComponent,
+        LocalLib.EventsListComponent,
+        LocalLib.EventsThumbnailComponent,
         NavbarComponent,
-        EventDetailsComponent,
-        CreateEventComponent,
+        LocalLib.EventDetailsComponent,
+        LocalLib.CreateEventComponent,
         Error404Component
     ],
     imports: [
@@ -30,10 +25,10 @@ import {EventsListResolver} from "./events/events-list-resolver.service";
         RouterModule.forRoot(appRoutes)
     ],
     providers: [
-        EventService,
+        LocalLib.EventService,
         ToastrService,
-        EventRouteActivator,
-        EventsListResolver,
+        LocalLib.EventRouteActivator,
+        LocalLib.EventsListResolver,
         { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
     ],
     bootstrap: [EventsAppComponent]
@@ -41,7 +36,7 @@ import {EventsListResolver} from "./events/events-list-resolver.service";
 export class AppModule {
 }
 
-export function checkDirtyState(component: CreateEventComponent) {
+export function checkDirtyState(component: LocalLib.CreateEventComponent) {
     if (component.isDirty)
         return window.confirm('You have not saved this event, do you really want to cancel?');
 
--- ng-fundamentals/src/routes.ts
+++ ng-fundamentals/src/routes.ts
@@ -1,7 +1,7 @@
 import {EventsListComponent} from "./app/events/events-list.component";
 import {EventDetailsComponent} from "./app/events/event-details/event-details.component";
 import {Routes} from "@angular/router";
-import {CreateEventComponent} from "./app/create-event/create-event.component";
+import {CreateEventComponent} from "./app/events/create-event.component";
 import {Error404Component} from "./app/errors/errors.component";
 import {EventRouteActivator} from "./app/events/event-details/event-route-activator.service";
 import {EventsListResolver} from "./app/events/events-list-resolver.service";
@@ -11,5 +11,6 @@ export const appRoutes: Routes = [
     {path: 'events', component: EventsListComponent, resolve: {events: EventsListResolver}},
     {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     {path: '404', component: Error404Component},
-    {path: '', redirectTo: '/events', pathMatch: 'full'}
+    {path: '', redirectTo: '/events', pathMatch: 'full'},
+    {path: 'user', loadChildren: './user/user.module#UserModule'}
 ];
