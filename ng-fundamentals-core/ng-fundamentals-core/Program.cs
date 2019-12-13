9a78781c072c2ac12c523c6691cf1d3f3a01bf3a
Alexis Alulema <alexis.alulema@gmail.com>
2019-12-13 12:52:36 -0500

    Upgrade to Angular 8 and .NET Core 3.1

--- ng-fundamentals-core/ng-fundamentals-core/Program.cs
+++ ng-fundamentals-core/ng-fundamentals-core/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace ng_fundamentals_core
@@ -14,11 +15,14 @@ namespace ng_fundamentals_core
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
